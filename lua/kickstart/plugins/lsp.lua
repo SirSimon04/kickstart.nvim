38d53301f2c2cfe3dca3eed71ec0bf972d8cb582
Simon Engel <simon.engel01@sap.com>
2024-03-20 18:25:14 +0100

    Refactoring

--- lua/kickstart/plugins/lsp.lua
+++ lua/kickstart/plugins/lsp.lua
@@ -191,6 +191,7 @@ return { -- LSP Configuration & Plugins
     local ensure_installed = vim.tbl_keys(servers or {})
     vim.list_extend(ensure_installed, {
       'stylua', -- Used to format lua code
+      'cds-lsp',
     })
     require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
